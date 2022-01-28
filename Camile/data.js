db160fd06c4f6ea5ebb3670d507da9624255a75d
leanhtuan20122002@gmail.com <leanhtuan20122002@gmail.com>
2022-01-28 22:03:23 +0700

    camile

---
+++ Camile/data.js
@@ -0,0 +1,19 @@
+const champions = [
+    {
+      id: 1,
+      name: "Camille",
+      image: "https://kicdo.com/images/source/champion/Camille.png",
+      lane: ["top"],
+      price: { 
+          thl: 6300,
+          rp: 99,
+        },  
+      skill: {
+        Q: "Giao thức chuẩn xác",
+        W: "Đá quét chiến thuật",
+        E: "Bắn dây móc",
+        R: "Tối hậu thư",
+      },
+      nickname: "Bóng thép",  
+    },
+];
